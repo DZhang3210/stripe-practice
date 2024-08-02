77eb44473fdd50669d3c637407facb3ed3202ab6
DZhang3210 <53838139+DZhang3210@users.noreply.github.com>
2024-08-02 15:58:21 -0400

    new commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,39 @@
-import Image from "next/image";
+"use client";
+import CheckoutPage from "@/components/CheckoutPage";
+import convertToSubsurrency from "@/lib/convertToSubcurrency";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+if (process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY === undefined) {
+  throw new Error("NEXT_PUBLIC_STRIPE_PUBLIC_KEY is not defined");
+}
+
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string
+);
 
 export default function Home() {
-  return (
-    <div>
+  const amount = 49.99;
 
-    </div>
+  return (
+    <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-blue-500 to-purple-500">
+      <div className="mb-10">
+        <h1 className="text-4xl font-extrabold mb-2">Sonny</h1>
+        <h2 className="text-2xl">
+          has requested
+          <span className="font-bold"> ${amount}</span>
+        </h2>
+      </div>
+      <Elements
+        stripe={stripePromise}
+        options={{
+          mode: "payment",
+          amount: convertToSubsurrency(amount), //cents
+          currency: "usd",
+        }}
+      >
+        <CheckoutPage amount={amount} />
+      </Elements>
+    </main>
   );
 }
